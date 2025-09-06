aa39c5c3c9d680b1003c7d43e35d1594a0466278
Robin Linacre <robinlinacre@hotmail.com>
2025-09-06 20:14:54 +0100

    Refactor real data matching logic to improve output clarity and debugging

--- try_real_data.py
+++ try_real_data.py
@@ -1,6 +1,8 @@
+import duckdb
 from matcher.get_data import (
     get_address_data_from_messy_address,
     get_random_address_data,
+    OS_PARQUET,
 )
 from matcher.trie_builder import build_trie_from_canonical, print_trie
 from matcher.matcher_stage1 import (
@@ -31,7 +33,24 @@ def log(msg: str) -> None:
 # Extract the messy tokens from the input row and run Stage‑1 matcher
 messy_tokens = list(messy_address[1])
 print("\n=== Stage‑1 match on real data ===")
-print("Messy clean:", " ".join(messy_tokens))
-res = match_stage1(messy_tokens, root, Params(), debug=log)
+print("Messy cleaned:", " ".join(messy_tokens) + " " + messy_address[2])
+res = match_stage1(messy_tokens, root, Params(), debug=False)
 
-" ".join([a for a in canonical_addresses if a[0] == res["uprn"]][0][1])
+if res["matched"]:
+    print("Match:")
+    print(" ".join([a for a in canonical_addresses if a[0] == res["uprn"]][0][1]))
+else:
+    print("no match")
+
+# in_postcode = [c for c in canonical_addresses if c[2] == messy_address[2]]
+# [(a[0], " ".join(a[1])) for a in in_postcode]
+
+# import duckdb
+# ddbdf = duckdb.read_parquet(OS_PARQUET)
+# sql = f"""
+# select fulladdress
+# from ddbdf
+# where postcode = '{messy_address[2]}'
+# """
+# duckdb.sql(sql).show()
+# res
